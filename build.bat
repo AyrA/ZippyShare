4ad1d52ab981b033bb29291f6e2462dd2d20697b
Kevin Gut <kevin.gut@bitm.sg>
2018-10-26 20:32:11 +0200

    Fix error message in build script

--- build.bat
+++ build.bat
@@ -1,6 +1,6 @@
 @ECHO OFF
-DEL zippy.tar
-DEL zippy.host
+IF EXIST zippy.tar DEL zippy.tar
+IF EXIST zippy.host DEL zippy.host
 "C:\Program Files\7-Zip\7z.exe" a -- zippy.tar INFO zippy.php
 "C:\Program Files\7-Zip\7z.exe" a -- zippy.tar.gz zippy.tar
 DEL zippy.tar
